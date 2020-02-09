04cf28ebfb1bd39a22c1afadb88a063c867004a4
JRMfer <43999123+JRMfer@users.noreply.github.com>
2020-02-09 23:55:35 +0100

    finished comments

--- code/classes/agents.py
+++ code/classes/agents.py
@@ -96,13 +96,16 @@ class GangMemberB(BM):
 
     def step(self):
         """
-        Moves the gangster according a semi biased Levy model.
+        Moves the gang member according a Brownian Motion algorithm.
         """
         self.random_move()
         self.check_rivals()
 
     def check_rivals(self):
-
+        """
+        Check if any rivals are within the vision of the agent. If so,
+        update rivalry matrix.
+        """
         poss_agents = self.model.area.get_neighbors(self.pos, self.vision,
                                                     include_center=True)
 
@@ -122,7 +125,10 @@ class SBLN(Agent):
                     weight_home, bounded_pareto, kappa, beta):
         """
         Each agent is initialized at its home location (set space) and
-        the Gang's id
+        the number of gang, a minimum jump length, a certaing weight constant
+        toward the home location and certain constant values for drawing random
+        numbers from a Von Mises distribution (bounded_pareto and kappa) and a
+        probabilty for crossing boundaries.
         """
 
         super().__init__(unique_id, model)
@@ -136,6 +142,7 @@ class SBLN(Agent):
 
     def move_SBLN(self):
         """
+        Semi-biased Levy walking algorithm
         """
 
         while True:
@@ -157,6 +164,7 @@ class SBLN(Agent):
             bias_home_x, bias_home_y = self.bias_home(gang_info)
             bias_rivals_x, bias_rivals_y = self.bias_rivals(gang_info)
 
+            # Determines overall bias
             bias_x = bias_home_x + bias_rivals_x
             bias_y = bias_home_y + bias_rivals_y
             bias = 0
@@ -170,28 +178,43 @@ class SBLN(Agent):
             change_x = jump * math.cos(angle_bias)
             change_y = jump * math.sin(angle_bias)
             old_region = self.model.config.areas[(int(x), int(y))]
+
+            # Determine the new postion
             x += change_x
             y += change_y
 
+            # Ensure new postion is inside area
             if not self.model.area.out_of_bounds((x, y)):
+
+                # Get new region, if new region is boundary then algorithms starts over
                 new_region = self.model.config.areas[(int(x), int(y))]
                 if new_region == 24:
                     continue
 
+                # Determine the amount of boundaries the agent needs to cross,
+                # and the chance the agent will make this move
                 boundaries = self.model.config.boundaries[
                                     old_region,
                                     new_region
                                     ]
                 chance = self.beta ** boundaries
 
+                # Checks if agent makes the move.
                 if random.uniform(0, 1) < chance:
                     self.model.area.move_agent(self, (x, y))
-                    # self.model.grid.move_agent(self, (int(x), int(y)))
+
+                # End algorithm
                 break
 
     def bias_home(self, gang_info):
         """
+        Determines the bias towards the member's own set space.
+        Input:
+            gang_info = list of Gang objects
+        Output:
+            A tuple containing the bias in the x and y coordinates towards home.
         """
+
         # Find norm "home vector"
         x_home, y_home = gang_info[self.number].coords
         bias_home_x = x_home - self.pos[0]
@@ -199,7 +222,7 @@ class SBLN(Agent):
         norm_home = math.sqrt(bias_home_x * bias_home_x +
                               bias_home_y * bias_home_y)
 
-        # Determine bias toawards home
+        # Determine bias towards home
         if norm_home:
             bias_home_x /= norm_home
             bias_home_y /= norm_home
@@ -216,16 +239,31 @@ class SBLN(Agent):
 
     def bias_rivals(self, gang_info):
         """
+        Determines the bias towards the rivals' set spaces.
+        Input:
+            gang_info = list of Gang objects
+        Output:
+            A tuple containing the bias in the x and y coordinates towards the rivals.
         """
+
+        # Determine the total interaction of member's gang
         bias_rivals_x, bias_rivals_y = 0.0, 0.0
         contact_all_rivals = self.model.rivalry_matrix[self.number, :].sum()
+
+        # Starts calculations for bias towards all rivals
         for gang in gang_info.values():
+
+            # Determines the amount of interaction of this member with another gang
             contact_rival = self.model.rivalry_matrix[self.number,
                                                       gang.number]
+
+            # Determine the normilzed vectory pointing to rival's set space
             rival_x = gang.coords[0] - self.pos[0]
             rival_y = gang.coords[1] - self.pos[1]
             norm_rival = math.sqrt(rival_x * rival_x + rival_y * rival_y)
 
+            # if there has been any contact then determine bias towards this specific
+            # gang
             if contact_all_rivals and norm_rival:
                 weight_bias = (-(contact_rival / contact_all_rivals)
                                / norm_rival)
@@ -264,6 +302,12 @@ class GangMember_SBLN(SBLN):
 
 
 class GRAV(SBLN):
+    """
+    An agent that moves according a gracity model. This agent determines its bias
+    based not only on all the set space of each gang and on the current status
+    of the rivalry matrix, but also on the size of each gang.
+    """
+
     def __init__(self, unique_id, model, pos, number,
                  min_jump, weight_home, bounded_pareto, kappa, beta):
         super().__init__(unique_id, model, pos, number,
@@ -272,6 +316,7 @@ class GRAV(SBLN):
 
     def move_SBLN(self):
         """
+        Gravity walking algorithm
         """
 
         while True:
@@ -293,6 +338,7 @@ class GRAV(SBLN):
             bias_home_x, bias_home_y = self.bias_home(gang_info)
             bias_rivals_x, bias_rivals_y = self.bias_rivals(gang_info)
 
+            # Determines bias agent
             bias_x = bias_home_x + bias_rivals_x
             bias_y = bias_home_y + bias_rivals_y
             bias = 0
@@ -306,25 +352,42 @@ class GRAV(SBLN):
             change_x = jump * math.cos(angle_bias)
             change_y = jump * math.sin(angle_bias)
             old_region = self.model.config.areas[(int(x), int(y))]
+
+            # Determines new position of agent
             x += change_x
             y += change_y
 
+            # Ensures new position is valid
             if not self.model.area.out_of_bounds((x, y)):
+
+                # Retrieve new region and ensures it is not a boundary.
+                # If boundary algorithm starts over
                 new_region = self.model.config.areas[(int(x), int(y))]
                 if new_region == 24:
                     continue
 
+                # Determines the amount of boundaries to cross and the
+                # corresponding chance an agent will make this move.
                 boundaries = self.model.config.boundaries[old_region,
                                                             new_region]
                 chance = self.beta ** boundaries
 
+                # Checks if agent makes move.
                 if random.uniform(0, 1) < chance:
                     self.model.area.move_agent(self, (x, y))
+
+                # End algorithm
                 break
 
     def bias_home(self, gang_info):
         """
+        Determines the bias towards the member's own set space.
+        Input:
+            gang_info = list of Gang objects
+        Output:
+            A tuple containing the bias in the x and y coordinates towards home.
         """
+
         # Find norm "home vector"
         x_home, y_home = gang_info[self.number].coords
         bias_home_x = x_home - self.pos[0]
@@ -332,7 +395,7 @@ class GRAV(SBLN):
         norm_home = math.sqrt(bias_home_x * bias_home_x +
                               bias_home_y * bias_home_y)
 
-        # Determine bias toawards home
+        # Determine bias towards home
         if norm_home:
             bias_home_x /= norm_home
             bias_home_y /= norm_home
@@ -349,16 +412,28 @@ class GRAV(SBLN):
 
     def bias_rivals(self, gang_info):
         """
+        Determines the bias towards the rivals' set spaces.
+        Input:
+            gang_info = list of Gang objects
+        Output:
+            A tuple containing the bias in the x and y coordinates towards the rivals.
         """
+
+        # Determines total interaction of member's gang
         bias_rivals_x, bias_rivals_y = 0.0, 0.0
         contact_all_rivals = self.model.rivalry_matrix[self.number, :].sum()
+
+        # Start calculation
         gang_inf = list(gang_info.values())
         for i in range(len(gang_inf)):
+
+            # Determines the normalized vector pointing towards a specific gang
             contact_rival = self.model.rivalry_matrix[self.number, i]
             rival_x = gang_inf[i].coords[0] - self.pos[0]
             rival_y = gang_inf[i].coords[1] - self.pos[1]
             norm_rival = math.sqrt(rival_x * rival_x + rival_y * rival_y)
 
+            # If contact and not already on set space of rival then determine bias
             if contact_all_rivals and norm_rival:
                 weight_bias = (-(contact_rival / contact_all_rivals)
                                / norm_rival) * (1 + self.model.norm_gang_size[i])
